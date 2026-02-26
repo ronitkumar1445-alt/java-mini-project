9e9347f54131f204fea5ce1eaf65e20f63c01c4e
ronitkumar1445-alt <ronitkumar1445@gmail.com>
2026-02-26 09:17:19 +0000

    file changed

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ let userSeq=[];
 
 let started=false;
 let level = 0;
-let h2= document.querySelectorAll("h2")
+let btns=["yellow","red","purple","green"];
+let h2= document.querySelector("h2")
 document.addEventListener("keypress",function(){
     if (started==false){
         console.log ("game is started");
      started==true;
-
      levelUp();
     }
-    function levelup(){
+    function btnFlash(btn){
+        btn.classList.add("flash");
+        setTimeout(function(){
+            btn.classList.remove("flash");},10000
+        );
+    }
+    function levelUp(){
         level++;
-     h2.innerText = 'level ${level}';
+     h2.innerText = `level ${level}`;
+     let rendIdx=Math.floor(Math.random()*3);
+     let randColor=btns[rendIdx];
+     let randbtn= document.querySelector(`.${randColor}`);
+     console.log(randIdx);
+     console.log(randColor);
+     console.log(randbtn);
+     btnFlash(randbtn);
     }
 });
 
