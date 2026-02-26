7f95c7ba0d969e94e6b9c56a84578b414e244566
ronitkumar1445-alt <ronitkumar1445@gmail.com>
2026-02-26 07:23:41 +0000

    file change

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ let userSeq=[];
 
 let started=false;
 let level = 0;
+let h2= document.querySelectorAll("h2")
 document.addEventListener("keypress",function(){
-    console.log("game started");
+    if (started==false){
+        console.log ("game is started");
+     started==true;
+
+     levelUp();
+    }
+    function levelup(){
+        level++;
+     h2.innerText = 'level ${level}';
+    }
 });
+
